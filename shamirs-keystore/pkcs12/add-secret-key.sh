23eb70e75c631bf4c5c8a543e69f85e1a99530e0
Christof Reichardt <chr78rm@onlinehome.de>
2020-04-17 15:08:16 +0200

    importpass replaced by genseckey when generating the aes key with keytool.

--- shamirs-keystore/pkcs12/add-secret-key.sh
+++ shamirs-keystore/pkcs12/add-secret-key.sh
@@ -3,5 +3,5 @@
 KEYSTORE_FILE=my-keystore-1.p12
 PASSWORD=Super-sicheres-Passwort
 
-keytool -importpass -alias my-aes-key -keypass $PASSWORD -keyalg AES -keysize 256 -keystore $KEYSTORE_FILE -storepass $PASSWORD -storetype pkcs12 -v
+keytool -genseckey -alias my-aes-key -keypass $PASSWORD -keyalg AES -keysize 256 -keystore $KEYSTORE_FILE -storepass $PASSWORD -storetype pkcs12 -v
 
