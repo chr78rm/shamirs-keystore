51a998e5eb78ded4d89dec4ebd7acf342a2fb612
Christof Reichardt <chr78rm@onlinehome.de>
2020-04-20 17:39:32 +0200

    Printing test plan and test results.

--- junit5-ext/src/main/java/de/christofreichardt/junit5/MyTestExecutionListener.java
+++ junit5-ext/src/main/java/de/christofreichardt/junit5/MyTestExecutionListener.java
@@ -24,6 +24,11 @@ import de.christofreichardt.diagnosis.LogLevel;
 import de.christofreichardt.diagnosis.Traceable;
 import de.christofreichardt.diagnosis.TracerFactory;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
@@ -34,6 +39,55 @@ import org.junit.platform.launcher.TestPlan;
  * @author christof.reichardt
  */
 public class MyTestExecutionListener implements TestExecutionListener, Traceable {
+
+	class Summary {
+		final String id;
+
+		int containers;
+		int tests;
+		int successful;
+		int failed;
+		int aborted;
+
+		public Summary(String id) {
+			this.id = id;
+		}
+
+		void incrementContainers() {
+			this.containers++;
+		}
+
+		void incrementTests() {
+			this.tests++;
+		}
+
+		void incrementsSuccessful() {
+			this.successful++;
+		}
+
+		void incrementFailed() {
+			this.failed++;
+		}
+
+		void incrementAborted() {
+			this.aborted++;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("id = %s, containers = %d, tests = %d, successful = %d, failed = %d, aborted = %d",
+					this.id, this.containers, this.tests, this.successful, this.failed, this.aborted);
+		}
+
+		void printSummary(PrintStream out) {
+			out.printf("%s, (uniqueId = %s)\n", MyTestExecutionListener.this.id2DisplayName.get(this.id), this.id);
+			out.printf("containers = %d, tests = %d, successful = %d, failed = %d, aborted = %d\n\n",
+					this.containers, this.tests, this.successful, this.failed, this.aborted);
+		}
+	}
+
+	Map<String, Summary> summaries = new LinkedHashMap<>();
+	Map<String, String> id2DisplayName = new HashMap<>();
 
 	@Override
 	public void testPlanExecutionStarted(TestPlan testPlan) {
@@ -55,11 +109,44 @@ public class MyTestExecutionListener implements TestExecutionListener, Traceable
 		} catch (TracerFactory.Exception ex) {
 			throw new RuntimeException("Tracer configuration failed,", ex);
 		}
+
+		traceTestPlan(testPlan);
+	}
+	void traceTestPlan(TestPlan testPlan) {
+		AbstractTracer tracer = getCurrentTracer();
+		tracer.entry("void", this, "traceTestPlan(TestPlan testPlan)");
+
+		try {
+			class TestPlanWalker {
+
+				void walk(TestIdentifier parent) {
+					tracer.out().printfIndentln("uniqueId = %s, displayName = %s", parent.getUniqueId(), parent.getDisplayName());
+					MyTestExecutionListener.this.id2DisplayName.put(parent.getUniqueId(), parent.getDisplayName());
+					for (TestIdentifier testIdentifier : testPlan.getChildren(parent)) {
+						walk(testIdentifier);
+					}
+				}
+			}
+
+			TestPlanWalker testPlanWalker = new TestPlanWalker();
+			for (TestIdentifier testIdentifier : testPlan.getRoots()) {
+				testPlanWalker.walk(testIdentifier);
+			}
+		} finally {
+			tracer.wayout();
+		}
 	}
 
 	@Override
 	public void testPlanExecutionFinished(TestPlan testPlan) {
 		System.out.printf("Testplan execution has been finished ...\n");
+
+		AbstractTracer tracer = getCurrentTracer();
+		tracer.out().println();
+		tracer.out().printfIndentln("-> Summary");
+		tracer.out().printfIndentln("==========");
+		this.summaries.entrySet()
+				.forEach(entry -> entry.getValue().printSummary(tracer.out()));
 
 		TracerFactory.getInstance().closePoolTracer();
 	}
@@ -72,7 +159,21 @@ public class MyTestExecutionListener implements TestExecutionListener, Traceable
 		try {
 			tracer.logMessage(LogLevel.INFO, String.format("%s started ...", testIdentifier.getDisplayName()),
 					getClass(), "executionStarted(TestIdentifier testIdentifier)");
-			tracer.out().printfIndentln("testIdentifier.getUniqueId() = %s", testIdentifier.getUniqueId());
+			tracer.out().printfIndentln("testIdentifier.getUniqueId() = %s, testIdentifier.getParentId() = %s", testIdentifier.getUniqueId(), testIdentifier.getParentId().orElse("[]"));
+			tracer.out().printfIndentln("testIdentifier.isContainer() = %b, testIdentifier.isTest() = %b",
+					testIdentifier.isContainer(), testIdentifier.isTest());
+
+			if (testIdentifier.getParentId().isPresent()) {
+				String parentId = testIdentifier.getParentId().get();
+				Summary summary = this.summaries.getOrDefault(parentId, new Summary(parentId));
+				if (testIdentifier.isContainer()) {
+					summary.incrementContainers();
+				}
+				if (testIdentifier.isTest()) {
+					summary.incrementTests();
+				}
+				this.summaries.putIfAbsent(parentId, summary);
+			}
 		} finally {
 			tracer.wayout();
 		}
@@ -105,6 +206,23 @@ public class MyTestExecutionListener implements TestExecutionListener, Traceable
 						}
 					} while (throwable != null);
 				});
+			}
+
+			if (testIdentifier.getParentId().isPresent()) {
+				Summary summary = this.summaries.get(testIdentifier.getParentId().get());
+				switch (testExecutionResult.getStatus()) {
+					case SUCCESSFUL:
+						summary.incrementsSuccessful();
+						break;
+					case FAILED:
+						summary.incrementFailed();
+						break;
+					case ABORTED:
+						summary.incrementAborted();
+						break;
+					default:
+						break;
+				}
 			}
 		} finally {
 			tracer.wayout();
