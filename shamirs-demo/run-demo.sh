59bada14155daa6571422bdef555c531044a043c
Christof Reichardt <chr78rm@onlinehome.de>
2020-05-12 15:39:25 +0200

    Moved to version 1.0.0-beta.

--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MAVEN_REPO=$HOME/.m2/repository/
-echo Using "${MAVEN_REPO}" ...
+VERSION=1.0.0-beta
+echo Using "${MAVEN_REPO}" and "${VERSION}" ...
 
 echo
 if [ "x${JAVA_HOME}" == "x" ]
@@ -12,10 +13,10 @@ if [ "x${JAVA_HOME}" == "x" ]
 fi
 ${BIN_JAVA} -version
 
-${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-0.0.1-SNAPSHOT.jar:\
+${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-${VERSION}.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/tracelogger/1.8.0/tracelogger-1.8.0.jar:\
-"${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/0.0.1-SNAPSHOT/shamirs-keystore-0.0.1-SNAPSHOT.jar:\
-"${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/0.0.1-SNAPSHOT/shamirs-secret-sharing-0.0.1-SNAPSHOT.jar:\
+"${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
+"${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
 "${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.1/scala-library-2.13.1.jar:\
 "${MAVEN_REPO}"/javax/json/javax.json-api/1.1.4/javax.json-api-1.1.4.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk15on/1.65/bcpkix-jdk15on-1.65.jar:\
