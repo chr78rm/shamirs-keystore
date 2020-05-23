2412cee5f1ff8610680f3e108bab857b06b175dd
Christof Reichardt <chr78rm@onlinehome.de>
2020-05-23 16:16:16 +0200

    Usage section complemented.

--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -17,8 +17,10 @@ ${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-${VERS
 "${MAVEN_REPO}"/de/christofreichardt/tracelogger/1.8.0/tracelogger-1.8.0.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.1/scala-library-2.13.1.jar:\
-"${MAVEN_REPO}"/javax/json/javax.json-api/1.1.4/javax.json-api-1.1.4.jar:\
+"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.2/scala-library-2.13.2.jar:\
+"${MAVEN_REPO}"/org/jboss/resteasy/resteasy-json-p-provider/resteasy-json-p-provider-4.5.3.Final.jar:\
+"${MAVEN_REPO}"/org/glassfish/jakarta.json/1.1.6/jakarta.json-1.1.6.jar:\
+"${MAVEN_REPO}"/org/jboss/logging/jboss-logging/3.3.2.Final/jboss-logging-3.3.2.Final.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk15on/1.65/bcpkix-jdk15on-1.65.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk15on/1.65/bcprov-jdk15on-1.65.jar:\
-"${MAVEN_REPO}"/org/glassfish/javax.json/1.1.4/javax.json-1.1.4.jar de.christofreichardt.jca.shamirsdemo.App
+ de.christofreichardt.jca.shamirsdemo.App
