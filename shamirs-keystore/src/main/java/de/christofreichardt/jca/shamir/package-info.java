edaacc1af2311ca261a0a1051f17da9e5525651e
Christof Reichardt <chr78rm@onlinehome.de>
2021-05-19 19:14:41 +0200

    Forgot the package-info.

---
+++ shamirs-keystore/src/main/java/de/christofreichardt/jca/shamir/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Comprises a JCA KeyStore engine and related classes,
+ * @see <a href="https://docs.oracle.com/en/java/javase/16/security/howtoimplaprovider.html#GUID-C485394F-08C9-4D35-A245-1B82CDDBC031">
+ *        How to Implement a Provider in the Java Cryptography Architecture
+ *      </a>
+ */
+package de.christofreichardt.jca.shamir;
