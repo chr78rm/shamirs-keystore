ceb2aed4a162160fa6c72275ddc1d612d4109e89
Christof Reichardt <chr78rm@onlinehome.de>
2022-01-18 14:16:28 +0100

    Bouncy Castle upgraded to 1.70.

--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -17,11 +17,11 @@ ${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-${VERS
 "${MAVEN_REPO}"/de/christofreichardt/tracelogger/1.8.0/tracelogger-1.8.0.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.7/scala-library-2.13.7.jar:\
+"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.8/scala-library-2.13.8.jar:\
 "${MAVEN_REPO}"/org/jboss/resteasy/resteasy-json-p-provider/resteasy-json-p-provider-4.5.3.Final.jar:\
 "${MAVEN_REPO}"/org/glassfish/jakarta.json/1.1.6/jakarta.json-1.1.6.jar:\
 "${MAVEN_REPO}"/org/jboss/logging/jboss-logging/3.3.2.Final/jboss-logging-3.3.2.Final.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk15on/1.69/bcpkix-jdk15on-1.69.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk15on/1.69/bcprov-jdk15on-1.69.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk15on/1.69/bcutil-jdk15on-1.69.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk15on/1.70/bcpkix-jdk15on-1.70.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk15on/1.70/bcprov-jdk15on-1.70.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk15on/1.70/bcutil-jdk15on-1.70.jar:\
  de.christofreichardt.jca.shamirsdemo.App
