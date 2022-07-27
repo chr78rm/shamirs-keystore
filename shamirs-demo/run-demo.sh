6fbdcebcd33d7890d5e3dc1916f4a02798838ef7
Christof Reichardt <chr78rm@onlinehome.de>
2022-07-27 18:13:58 +0200

    Dependencies upgraded.

--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAVEN_REPO=$HOME/.m2/repository/
-VERSION=1.2.0
+VERSION=1.2.1
 echo Using "${MAVEN_REPO}" and "${VERSION}" ...
 
 echo
@@ -18,11 +18,11 @@ ${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-${VERS
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
 "${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.8/scala-library-2.13.8.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala3-library_3/3.1.2/scala3-library_3-3.1.2.jar:\
+"${MAVEN_REPO}"/org/scala-lang/scala3-library_3/3.1.3/scala3-library_3-3.1.3.jar:\
 "${MAVEN_REPO}"/org/jboss/resteasy/resteasy-json-p-provider/resteasy-json-p-provider-4.5.3.Final.jar:\
 "${MAVEN_REPO}"/org/glassfish/jakarta.json/1.1.6/jakarta.json-1.1.6.jar:\
 "${MAVEN_REPO}"/org/jboss/logging/jboss-logging/3.3.2.Final/jboss-logging-3.3.2.Final.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk15on/1.70/bcpkix-jdk15on-1.70.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk15on/1.70/bcprov-jdk15on-1.70.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk15on/1.70/bcutil-jdk15on-1.70.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk18on/1.71/bcpkix-jdk18on-1.71.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk18on/1.71/bcprov-jdk18on-1.71.jar:\
+"${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk18on/1.71/bcutil-jdk18on-1.71.jar:\
  de.christofreichardt.jca.shamirsdemo.App
