0f0baea9c9640bd5913b03ab1c4c628c29a34512
Christof Reichardt <chr78rm@onlinehome.de>
2023-06-28 16:42:28 +0200

    Removed experimental files related to JSON web signature.

--- shamirs-secret-sharing/src/test/scala/de/christofreichardt/scala/shamir/ShamirSuites.scala
+++ shamirs-secret-sharing/src/test/scala/de/christofreichardt/scala/shamir/ShamirSuites.scala
@@ -20,7 +20,6 @@
 package de.christofreichardt.scala.shamir
 
 import de.christofreichardt.scala.combinations.BinomialCombinatorSuite
-import de.christofreichardt.scala.jws.JsonWebSignatureSuite
 import de.christofreichardt.scalatest.{MyDummySuite, MySuites}
 
 class ShamirSuites extends MySuites(
@@ -30,7 +29,6 @@ class ShamirSuites extends MySuites(
     new PolynomialSuite(),
     new SecretSharingSuite(),
     new SecretMergingSuite(),
-    new JsonWebSignatureSuite(),
     new BinomialCombinatorSuite()) {
   
 }
