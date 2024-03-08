a4cc57476c81372fc49a6124e4c2cba90eb91443
Christof Reichardt <chr78rm@onlinehome.de>
2024-03-08 14:20:21 +0100

    Classpath updated.

--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -17,12 +17,10 @@ ${BIN_JAVA} -Djava.security.egd=file:/dev/urandom -cp target/shamirs-demo-${VERS
 "${MAVEN_REPO}"/de/christofreichardt/tracelogger/1.10.0/tracelogger-1.10.0.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
 "${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.10/scala-library-2.13.10.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala3-library_3/3.3.1/scala3-library_3-3.3.1.jar:\
+"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.12/scala-library-2.13.12.jar:\
+"${MAVEN_REPO}"/org/scala-lang/scala3-library_3/3.3.3/scala3-library_3-3.3.3.jar:\
 "${MAVEN_REPO}"/jakarta/json/jakarta.json-api/2.1.3/jakarta.json-api-2.1.3.jar:\
 "${MAVEN_REPO}"/org/eclipse/parsson/jakarta.json/1.1.5/jakarta.json-1.1.5.jar:\
-"${MAVEN_REPO}"/org/glassfish/jakarta.json/1.1.6/jakarta.json-1.1.6.jar:\
-"${MAVEN_REPO}"/org/jboss/logging/jboss-logging/3.3.2.Final/jboss-logging-3.3.2.Final.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk18on/1.77/bcpkix-jdk18on-1.77.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk18on/1.77/bcprov-jdk18on-1.77.jar:\
 "${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk18on/1.77/bcutil-jdk18on-1.77.jar:\
