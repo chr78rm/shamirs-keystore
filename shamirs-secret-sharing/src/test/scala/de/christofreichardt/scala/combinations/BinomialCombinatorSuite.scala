1c6e78442bce1d16fa8f4ccf7aec0406ed69df66
Christof Reichardt <chr78rm@onlinehome.de>
2024-03-08 13:43:12 +0100

    Copyright notice updated.

--- shamirs-secret-sharing/src/test/scala/de/christofreichardt/scala/combinations/BinomialCombinatorSuite.scala
+++ shamirs-secret-sharing/src/test/scala/de/christofreichardt/scala/combinations/BinomialCombinatorSuite.scala
@@ -1,7 +1,7 @@
 /*
  * Shamirs Keystore
  *
- * Copyright (C) 2017, 2022, Christof Reichardt
+ * Copyright (C) 2017, 2024, Christof Reichardt
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
