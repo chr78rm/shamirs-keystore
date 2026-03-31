8cd9aacb766a6a365edd48fda46c0f435a9f4556
Christof Reichardt <chr78rm@onlinehome.de>
2026-03-31 17:44:11 +0200

    Moved away from building an uber-jar by shading, using instead the dependency and jar plugins to copy the required dependencies into 'target/lib' and handle manifest configuration accordingly.

--- shamirs-demo/make-distribution.sh
+++ shamirs-demo/make-distribution.sh
@@ -7,25 +7,16 @@ NAME=$2
 echo "Creating distribution shamirs-demo-${VERSION} ..."
 
 echo "Making directories as needed ..."
-mkdir --parents ${DIST}/shamirs-demo
-mkdir --parents ${DIST}/shamirs-demo/log
-mkdir --parents ${DIST}/shamirs-demo/target
-mkdir --parents ${DIST}/shamirs-demo/workspace
-mkdir --parents ${DIST}/shamirs-demo/src
+mkdir --parents ${DIST}/shamirs-demo/log ${DIST}/shamirs-demo/target/lib ${DIST}/shamirs-demo/workspace ${DIST}/shamirs-demo/src
 
 echo "Copying sources ..."
 cp --recursive src/main ${DIST}/shamirs-demo/src
 
-echo "Copying binary ..."
+echo "Copying binaries ..."
 cp ./target/${NAME}-${VERSION}.jar ${DIST}/shamirs-demo/target
+cp ./target/lib/*.jar ${DIST}/shamirs-demo/target/lib
 
 echo "Creating launch scripts ..."
-cp ./run-uber-demo.sh ${DIST}/shamirs-demo/run-shamirs-demo.sh
-cp ./run-uber-demo.bat ${DIST}/shamirs-demo/run-shamirs-demo.bat
-# winpty wrapper
-echo '#!/bin/bash' > ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
-echo '' >> ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
-echo '# you must quote the arguments which are passed through, like:' >> ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
-echo '# ./winpty-shamirs-demo.sh "--no-bulk --jline --echo"' >> ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
-echo '' >> ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
-echo 'winpty sh -i -c "./run-shamirs-demo.sh $@"' >> ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
+cp ./run-demo.sh ${DIST}/shamirs-demo/run-shamirs-demo.sh
+cp ./run-demo.bat ${DIST}/shamirs-demo/run-shamirs-demo.bat
+cp ./winpty-demo.sh ${DIST}/shamirs-demo/winpty-shamirs-demo.sh
--- shamirs-demo/run-demo.sh
+++ shamirs-demo/run-demo.sh
@@ -3,10 +3,6 @@
 set -o errexit # terminate on error
 ARGS=$* # all parameter
 
-MAVEN_REPO=/c/Users/Developer/.m2/repository
-VERSION=1.4.0
-echo Using "${MAVEN_REPO}" and "${VERSION}" ...
-
 # defaults
 ECHO_SWITCH=OFF
 BULK_SWITCH=ON
@@ -59,20 +55,10 @@ if [ "x${JAVA_HOME}" == "x" ]
 fi
 ${BIN_JAVA} -version
 
+SHAMIRS_VERSION=1.4.0
 ${BIN_JAVA} -Djava.security.egd=file:/dev/urandom \
--Dde.christofreichardt.jca.shamirsdemo.console.echo=${ECHO_SWITCH} \
-${CONSOLE_PROPERTY} \
--Dde.christofreichardt.jca.shamirsdemo.console.bulk=${BULK_SWITCH} \
-${SUN_MISC_UNSAFE_OPTION} \
--cp target/shamirs-demo-${VERSION}.jar:\
-"${MAVEN_REPO}"/de/christofreichardt/tracelogger/1.10.0/tracelogger-1.10.0.jar:\
-"${MAVEN_REPO}"/de/christofreichardt/shamirs-keystore/${VERSION}/shamirs-keystore-${VERSION}.jar:\
-"${MAVEN_REPO}"/de/christofreichardt/shamirs-secret-sharing/${VERSION}/shamirs-secret-sharing-${VERSION}.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala-library/2.13.16/scala-library-2.13.16.jar:\
-"${MAVEN_REPO}"/org/scala-lang/scala3-library_3/3.7.4/scala3-library_3-3.7.4.jar:\
-"${MAVEN_REPO}"/jakarta/json/jakarta.json-api/2.1.3/jakarta.json-api-2.1.3.jar:\
-"${MAVEN_REPO}"/org/eclipse/parsson/jakarta.json/1.1.7/jakarta.json-1.1.7.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcpkix-jdk18on/1.82/bcpkix-jdk18on-1.82.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcprov-jdk18on/1.82/bcprov-jdk18on-1.82.jar:\
-"${MAVEN_REPO}"/org/bouncycastle/bcutil-jdk18on/1.82/bcutil-jdk18on-1.82.jar\
- de.christofreichardt.jca.shamirsdemo.App
+  -Dde.christofreichardt.jca.shamirsdemo.console.echo=${ECHO_SWITCH} \
+  ${CONSOLE_PROPERTY} \
+  -Dde.christofreichardt.jca.shamirsdemo.console.bulk=${BULK_SWITCH} \
+  ${SUN_MISC_UNSAFE_OPTION} \
+  -jar target/shamirs-demo-${SHAMIRS_VERSION}.jar
